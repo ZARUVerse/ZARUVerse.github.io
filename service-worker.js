3a1fbf615acba56dac150998b4f41e69d441832f
ZARUverse <zaru.verse@makese.site>
2025-10-14 13:25:34 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,28 @@
-self.addEventListener("install", event => {
+// service-worker.js
+const CACHE = "zaru-cache-v1";
+const PRECACHE = [
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+  "https://zaruverse.github.io/lion-logo.png",
+  "https://zaruverse.github.io/zaru-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(PRECACHE)));
+});
+
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.open("zaru-cache").then(cache => {
-      return cache.addAll([
-        "/index.html",
-        "/style.css",
-        "/script.js",
-        "/manifest.json",
-        "https://zaruverse.github.io/zaru-512.png",
-        "https://zaruverse.github.io/lion-logo.png"
-      ]);
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
