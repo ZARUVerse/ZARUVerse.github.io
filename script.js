18f65673d65ba75abbd49939810ce9c138f937e1
ZARUverse <zaru.verse@makese.site>
2025-10-14 12:35:51 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 const tokens = {
-  zaru: "0xdac6f0ec5901b3877dc6db23c3882267ba9fabf6",
-  zarux: "0x7e8e9a6f626e499cf24063cdaf937615576b6cf3",
-  dmd: "0x8c4c0116d051f6f2ea27e8fa7614c8f799191ab3",
-  mak1: "0x9f833ca55ce0431045098ef12b64da6b33cf1f48",
-  makese: "0xa6bcd9434e8db84108612bb7ccb77775306adf1a",
-  btcbr: "0x40310d3d4eaeeffba63e39cd5b7b65bd51d23164",
-  bitcoinbr: "0x90bb20727b6e28c02cd941ecce635ffd826e3e00"
+  zaru: "0xdac6f0ec5901b3877dc6db23c3882267ba9fabf6"
+  // سایر توکن‌ها رو اضافه کن
 };
 
 Object.entries(tokens).forEach(([name, address]) => {
@@ -19,8 +14,5 @@ Object.entries(tokens).forEach(([name, address]) => {
           element.innerText = "$" + price.toFixed(4);
         }
       }
-    })
-    .catch(err => {
-      console.error(`Error fetching price for ${name}:`, err);
     });
 });
